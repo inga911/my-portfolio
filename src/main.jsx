69f3f3680ca1d621f250697bd542d51b989ec3d8
inga911 <inga.banaityte221@gmail.com>
2023-10-05 12:49:16 +0200

    back to top

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import BackToTop from "./components/back-to-top/Back-to-top.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <BackToTop />
   </React.StrictMode>
 );
